4d8f8bcf2f1bf3676c1e57ddefc1b0fc13896853
LABELNET <1406046087@qq.com>
2016-05-09 09:57:04 +0800

    【EDIT】添加BaseFragment和初始化baseActivity

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/main/BaseActivity.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/main/BaseActivity.java
@@ -2,7 +2,12 @@ package cn.edu.hpu.yuan.yuannews.main;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import cn.edu.hpu.yuan.yuannews.R;
 
 /**
  * Created by yuan on 16-5-9.
@@ -11,10 +16,27 @@ import android.support.v7.app.AppCompatActivity;
  * 2.初始化fragment
  * 3.
  */
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.base_main);
+
+        //初始化toolbar
+        final Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        //初始化fragment
+        BaseFragment fragment=initFragment();
+        FragmentManager manager=getSupportFragmentManager();
+        manager.beginTransaction().add(R.id.contentFrame, fragment).commit();
     }
+
+    //返回fragment
+    protected abstract BaseFragment initFragment();
+
 }
---
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/main/BaseFragment.java
@@ -0,0 +1,11 @@
+package cn.edu.hpu.yuan.yuannews.main;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by yuan on 16-5-9.
+ * Fragment 基类
+ * 1.为广大的fragment提供支持
+ */
+public class BaseFragment extends Fragment{
+}
