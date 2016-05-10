8eb7e57f9b8824932dcfe3a711760a8a8d2dddec
LABELNET <1406046087@qq.com>
2016-05-10 15:47:33 +0800

    【EDIT】添加MainFragment

---
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/main/MainFragment.java
@@ -0,0 +1,33 @@
+package cn.edu.hpu.yuan.yuannews.news.main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.edu.hpu.yuan.yuannews.main.base.BaseFragment;
+
+/**
+ * Created by yuan on 16-5-10.
+ * 1.主界面：初始化加载，分类和来源信息到tabs上
+ * 2.。。。
+ */
+public class MainFragment extends BaseFragment{
+
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+}
