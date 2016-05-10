4ce6dac300634613110e9cd7a350b444e951600b
LABELNET <1406046087@qq.com>
2016-05-10 19:06:48 +0800

    【EDIT】实现主页注入器Component

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/main/MainComponent.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/main/MainComponent.java
@@ -1,8 +1,18 @@
 package cn.edu.hpu.yuan.yuannews.news.main;
 
+import cn.edu.hpu.yuan.yuannews.main.scope.PerActivity;
+import dagger.Component;
+
 /**
  * Created by yuan on 16-5-10.
  * 注入器，两个地方：Activity和Fragment注入实例
  */
-public class MainComponent {
+@PerActivity
+@Component(modules = MainModule.class)
+public interface MainComponent {
+
+    void injectMainActivity(MainActivity mainActivity);
+
+    void injectMainFragment(MainFragment mainFragment);
+
 }
