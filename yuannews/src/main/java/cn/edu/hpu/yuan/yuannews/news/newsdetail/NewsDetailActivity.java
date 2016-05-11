fc85ced7114afacbb6060b61cffc0eb3a7b14c97
LABELNET <1406046087@qq.com>
2016-05-11 20:08:01 +0800

    【EDIT】配置详情页Activity

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/newsdetail/NewsDetailActivity.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/newsdetail/NewsDetailActivity.java
@@ -1,8 +1,32 @@
 package cn.edu.hpu.yuan.yuannews.news.newsdetail;
 
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import cn.edu.hpu.yuan.yuannews.main.base.BaseFragment;
+import cn.edu.hpu.yuan.yuannews.main.base.NormalBackActivity;
+
 /**
  * Created by yuan on 16-5-11.
  * 新闻详情页Activity
  */
-public class NewsDetailActivity {
+public class NewsDetailActivity extends NormalBackActivity{
+
+
+    @Override
+    protected void initView(Bundle savedInstanceState, Toolbar toolbar) {
+
+    }
+
+    @Override
+    protected BaseFragment initFragment() {
+        return null;
+    }
+
+    @Override
+    protected void setComponent() {
+
+    }
+
+
 }
