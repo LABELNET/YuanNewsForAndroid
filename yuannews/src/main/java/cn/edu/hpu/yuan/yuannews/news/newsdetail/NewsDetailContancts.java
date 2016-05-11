1da289e8a9e42ec06dff130a8c95fa7854e8b131
LABELNET <1406046087@qq.com>
2016-05-11 21:28:29 +0800

    【EDIT】实现Contanst

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/newsdetail/NewsDetailContancts.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/newsdetail/NewsDetailContancts.java
@@ -1,7 +1,43 @@
 package cn.edu.hpu.yuan.yuannews.news.newsdetail;
 
+import java.util.List;
+
+import cn.edu.hpu.yuan.yuannews.main.data.model.basevo.LikedVo;
+import cn.edu.hpu.yuan.yuannews.main.data.model.news.NewsCustom;
+
 /**
  * Created by yuan on 16-5-11.
+ * Presenter
  */
-public class NewsDetailContancts {
+public interface NewsDetailContancts {
+
+
+    interface NewsDetailPresenter{
+
+        void getNewsDetailData();
+
+        void updateNewsZan();
+
+        void getNewsZansHeadData();
+    }
+
+    interface NewsDetailView{
+
+        void showDialog();
+
+        void getNewsDetail(NewsCustom newsCustom);
+
+        void loadError();
+
+        void dimssDialog();
+
+        void updateZanSuccess();
+
+        void updateZanError();
+
+        void getZansHead(List<LikedVo> likedVos);
+
+        void getZansHeadError();
+    }
+
 }
--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/newsdetail/NewsDetailPresenter.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/newsdetail/NewsDetailPresenter.java
@@ -3,5 +3,5 @@ package cn.edu.hpu.yuan.yuannews.news.newsdetail;
 /**
  * Created by yuan on 16-5-11.
  */
-public class NewsDetailPresenter {
+public class NewsDetailPresenter{
 }
