fe8ec222c37a584e158d005d08c691329028a806
LABELNET <1406046087@qq.com>
2016-05-11 20:14:40 +0800

    【EDIT】显示状态栏

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/main/base/BaseActivity.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/main/base/BaseActivity.java
@@ -61,7 +61,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
         setContentView(R.layout.base_main);
         setComponent();
 
