fb112af66c4bf1f0d59e8f34f212e76a86f2bf0f
LABELNET <1406046087@qq.com>
2016-05-12 09:43:44 +0800

    【EDIT】activity实现注入Fragment

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/user/login/LoginActivity.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/user/login/LoginActivity.java
@@ -1,7 +1,39 @@
 package cn.edu.hpu.yuan.yuannews.user.login;
 
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import javax.inject.Inject;
+
+import cn.edu.hpu.yuan.yuannews.main.base.BaseFragment;
+import cn.edu.hpu.yuan.yuannews.main.base.NormalBackActivity;
+
 /**
  * Created by yuan on 16-5-12.
  */
-public class LoginActivity {
+public class LoginActivity extends NormalBackActivity{
+
+
+    @Inject
+    protected LoginFragment loginFragment;
+
+
+    @Override
+    protected void initView(Bundle savedInstanceState, Toolbar toolbar) {
+        toolbar.setTitle("用户登陆");
+    }
+
+    @Override
+    protected BaseFragment initFragment() {
+        return loginFragment;
+    }
+
+    @Override
+    protected void setComponent() {
+        DaggerLoginComponent
+                .builder()
+                .loginModule(new LoginModule())
+                .build()
+                .injectLoginActivity(this);
+    }
 }
--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/user/login/LoginFragment.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/user/login/LoginFragment.java
@@ -1,7 +1,28 @@
 package cn.edu.hpu.yuan.yuannews.user.login;
 
+import android.os.Bundle;
+import android.view.View;
+
+import cn.edu.hpu.yuan.yuannews.main.base.NorbalBackFragment;
+
 /**
  * Created by yuan on 16-5-12.
  */
-public class LoginFragment {
+public class LoginFragment extends NorbalBackFragment{
+
+
+    @Override
+    protected void onloadData() {
+
+    }
+
+    @Override
+    protected void initView(View view, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initComponent() {
+
+    }
 }
