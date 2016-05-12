756c6186be091be1c460ea7fd582f72be409e22b
LABELNET <1406046087@qq.com>
2016-05-13 00:16:36 +0800

    【EDIT】添加关注标签页面

---
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/labels/LabelsActivity.java
@@ -0,0 +1,8 @@
+package cn.edu.hpu.yuan.yuannews.news.labels;
+
+/**
+ * Created by yuan on 16-5-13.
+ * 所有标签页，分页加载实现
+ */
+public class LabelsActivity {
+}
---
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/labels/LabelsFragment.java
@@ -0,0 +1,7 @@
+package cn.edu.hpu.yuan.yuannews.news.labels;
+
+/**
+ * Created by yuan on 16-5-13.
+ */
+public class LabelsFragment {
+}
