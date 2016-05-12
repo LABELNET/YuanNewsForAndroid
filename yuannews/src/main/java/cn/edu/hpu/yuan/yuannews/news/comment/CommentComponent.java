3d2572316924af3974297c257ef0c98d4ba0661a
LABELNET <1406046087@qq.com>
2016-05-13 00:42:01 +0800

    【EDIT】注册Activity实现

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/comment/CommentComponent.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/comment/CommentComponent.java
@@ -1,7 +1,15 @@
 package cn.edu.hpu.yuan.yuannews.news.comment;
 
+import dagger.Component;
+
 /**
  * Created by yuan on 16-5-13.
  */
-public class CommentComponent {
+@Component(modules = CommentModule.class)
+public interface CommentComponent {
+
+    void injectCommentActivity(CommenActivity commenActivity);
+    
+    void injectCommentFragment(CommentFragment commentFragment);
+
 }
