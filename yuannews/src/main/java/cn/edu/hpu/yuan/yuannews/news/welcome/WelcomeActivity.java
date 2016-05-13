b61bc317626dadb967f69b5343da7515a1dc8916
LABELNET <1406046087@qq.com>
2016-05-13 18:44:25 +0800

    【EDIT】添加欢迎页面

---
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/welcome/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package cn.edu.hpu.yuan.yuannews.news.welcome;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yuan on 16-5-13.
+ * 欢迎页面
+ */
+public class WelcomeActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+}
