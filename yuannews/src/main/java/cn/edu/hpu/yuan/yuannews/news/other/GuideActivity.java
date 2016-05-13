03b68a86df54b4ee4869a4438f26159a23416fef
LABELNET <1406046087@qq.com>
2016-05-13 22:41:33 +0800

    【EDIT】添加欢迎页面

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/other/GuideActivity.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/other/GuideActivity.java
@@ -1,8 +1,18 @@
 package cn.edu.hpu.yuan.yuannews.news.other;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by yuan on 16-5-13.
  * 引导页
  */
-public class GuideActivity {
+public class GuideActivity extends AppCompatActivity{
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/other/WelcomeActivity.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/other/WelcomeActivity.java
@@ -1,8 +1,14 @@
 package cn.edu.hpu.yuan.yuannews.news.other;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import cn.edu.hpu.yuan.yuannews.R;
+import cn.edu.hpu.yuan.yuannews.news.main.MainActivity;
 
 /**
  * Created by yuan on 16-5-13.
@@ -10,9 +16,22 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class WelcomeActivity extends AppCompatActivity{
 
+
+    Handler handler= new Handler();
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
+        setContentView(R.layout.wlecome_activity);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
+            }
+        },3000);
     }
 
 }
