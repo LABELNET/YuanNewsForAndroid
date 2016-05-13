83436e53292630ac9ada02a32ce4f6766ab71e8d
LABELNET <1406046087@qq.com>
2016-05-13 23:06:08 +0800

    【EDIT】注册推荐新闻Activity

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/tuijian/TuijianActivity.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/tuijian/TuijianActivity.java
@@ -1,7 +1,33 @@
 package cn.edu.hpu.yuan.yuannews.news.tuijian;
 
+import android.support.v7.widget.Toolbar;
+
+import cn.edu.hpu.yuan.yuannews.main.base.BaseFragment;
+import cn.edu.hpu.yuan.yuannews.main.base.NormalBaseActivity;
+
 /**
  * Created by yuan on 16-5-13.
  */
-public class TuijianActivity {
+public class TuijianActivity extends NormalBaseActivity{
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void setComponet() {
+
+    }
+
+    @Override
+    protected void initToolbar(Toolbar toolbar) {
+
+    }
+
+    @Override
+    protected BaseFragment initFragment() {
+        return null;
+    }
+
 }
--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/tuijian/TuijianFragment.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/tuijian/TuijianFragment.java
@@ -1,7 +1,21 @@
 package cn.edu.hpu.yuan.yuannews.news.tuijian;
 
+import android.os.Bundle;
+import android.view.View;
+
+import cn.edu.hpu.yuan.yuannews.main.base.NorbalBackFragment;
+
 /**
  * Created by yuan on 16-5-13.
  */
-public class TuijianFragment {
+public class TuijianFragment extends NorbalBackFragment{
+    @Override
+    protected void initView(View view, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initComponent() {
+
+    }
 }
