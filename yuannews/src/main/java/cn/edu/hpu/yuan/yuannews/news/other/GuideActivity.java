15b09a5d03e773537c63f8687583a467ea1744e1
LABELNET <1406046087@qq.com>
2016-05-13 18:45:48 +0800

    【EDIT】添加引导页Activity

---
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/other/GuideActivity.java
@@ -0,0 +1,8 @@
+package cn.edu.hpu.yuan.yuannews.news.other;
+
+/**
+ * Created by yuan on 16-5-13.
+ * 引导页
+ */
+public class GuideActivity {
+}
--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/other/WelcomeActivity.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/other/WelcomeActivity.java
@@ -1,4 +1,4 @@
-package cn.edu.hpu.yuan.yuannews.news.welcome;
+package cn.edu.hpu.yuan.yuannews.news.other;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
