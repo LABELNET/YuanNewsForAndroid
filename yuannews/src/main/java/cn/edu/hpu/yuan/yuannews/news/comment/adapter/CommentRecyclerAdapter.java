147691bdbc2d591a4f9061edba55b0329115d756
LABELNET <1406046087@qq.com>
2016-05-13 16:38:35 +0800

    【EDIT】添加评论列表item

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/comment/adapter/CommentRecyclerAdapter.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/comment/adapter/CommentRecyclerAdapter.java
@@ -4,12 +4,28 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.hpu.yuan.yuannews.main.data.model.basevo.CommentJo;
+
+
 /**
  * Created by yuan on 16-5-13.
  * 在create的时候通过 tag 传递binding
  */
 public class CommentRecyclerAdapter  extends RecyclerView.Adapter<CommentRecyclerAdapter.CommentViewAdapter>{
 
+
+    private final List<CommentJo> commentJos=new ArrayList<>();
+
+    public void addComemntJo(ArrayList<CommentJo> commentJos){
+        commentJos.addAll(commentJos);
+    }
+
+    public void clearComemntJos(){
+        commentJos.clear();
+    }
 
     @Override
     public CommentViewAdapter onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -23,7 +39,7 @@ public class CommentRecyclerAdapter  extends RecyclerView.Adapter<CommentRecycle
 
     @Override
     public int getItemCount() {
-        return 0;
+        return commentJos.size();
     }
 
     class CommentViewAdapter extends RecyclerView.ViewHolder{
@@ -32,4 +48,5 @@ public class CommentRecyclerAdapter  extends RecyclerView.Adapter<CommentRecycle
             super(itemView);
         }
     }
+
 }
