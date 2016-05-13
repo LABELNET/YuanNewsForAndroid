74f4352e55e8e9533d795b9ef5dc6c530c11e27c
LABELNET <1406046087@qq.com>
2016-05-13 16:45:28 +0800

    【EDIT】添加评论列表Adapter实现

--- yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/comment/adapter/CommentRecyclerAdapter.java
+++ yuannews/src/main/java/cn/edu/hpu/yuan/yuannews/news/comment/adapter/CommentRecyclerAdapter.java
@@ -1,12 +1,20 @@
 package cn.edu.hpu.yuan.yuannews.news.comment.adapter;
 
+import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.edu.hpu.yuan.yuannews.R;
+import cn.edu.hpu.yuan.yuannews.databinding.CommentsFragmentItemBinding;
+import cn.edu.hpu.yuan.yuannews.main.data.NewsAPI;
 import cn.edu.hpu.yuan.yuannews.main.data.model.basevo.CommentJo;
 
 
@@ -27,14 +35,30 @@ public class CommentRecyclerAdapter  extends RecyclerView.Adapter<CommentRecycle
         commentJos.clear();
     }
 
+    private LayoutInflater inflater;
+    private Context context;
+
+    public CommentRecyclerAdapter(Context context) {
+        this.inflater = LayoutInflater.from(context);
+        this.context=context;
+    }
+
     @Override
     public CommentViewAdapter onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        CommentsFragmentItemBinding binding= DataBindingUtil.inflate(inflater, R.layout.comments_fragment_item,parent,false);
+        return new CommentViewAdapter(binding.getRoot());
     }
 
     @Override
     public void onBindViewHolder(CommentViewAdapter holder, int position) {
-
+        CommentsFragmentItemBinding binding=DataBindingUtil.bind(holder.itemView);
+        CommentJo commentJo = commentJos.get(position);
+        binding.setCommentJo(commentJo);
+        Glide.with(context)
+                .load(NewsAPI.BASE_IMAGE_URL+commentJo.getHead())
+                .error(R.mipmap.ic_news)
+                .placeholder(R.mipmap.user_head)
+                .into(binding.profileImage);
     }
 
     @Override
@@ -43,7 +67,6 @@ public class CommentRecyclerAdapter  extends RecyclerView.Adapter<CommentRecycle
     }
 
     class CommentViewAdapter extends RecyclerView.ViewHolder{
-
         public CommentViewAdapter(View itemView) {
             super(itemView);
         }
